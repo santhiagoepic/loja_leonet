31e1765d870932fb1de2a703d9b39bcfd649f8bd
santhiagoepic <ms1442877@gmail.com>
2025-10-02 11:57:19 -0300

    crxemulator="i"

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br" crxemulator="i">
       <body>
         <Header />
         {children}
